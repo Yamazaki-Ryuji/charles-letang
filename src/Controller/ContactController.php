4ec25d3639959467707743bf8e3c2f6e6a921cd2
Yamazaki-Ryuji <genjuro23000@gmail.com>
2022-06-04 15:04:14 +0200

    css

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,20 +2,53 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer)
     {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+            
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('letang.charles@orange.fr')
+                ->subject('Charles Létang')
+                ->text('Message de '.$contactFormData['fullName'].\PHP_EOL
+                    .$contactFormData['email'].\PHP_EOL
+                    .\PHP_EOL
+                    .$contactFormData['message'],
+                    'text/plain');
+                    $mailer->send($message);
+
+
+
+
+            $this->addFlash('success', 'Votre message a été envoyé');
+
+            return $this->redirectToRoute('contact');
+        }
+
+
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'our_form' => $form->createView()
         ]);
     }
+    
 }
