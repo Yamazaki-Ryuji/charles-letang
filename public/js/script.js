47874b2bfa9c09745d58d16867fc5b18a7be58a3
Charles Letang <94009414+Yamazaki-Ryuji@users.noreply.github.com>
2022-06-06 16:52:04 +0200

    skills css js

--- public/js/script.js
+++ public/js/script.js
@@ -18,3 +18,38 @@ links.forEach((link) => {
       cursor.classList.remove('link');
    })
 })
+
+/************** Sphere *************/
+
+const myTags = [
+   'JavaScript', 'CSS', 'HTML',
+   'Python', 'Java', 'git',
+   'Node.js', 'MySQL', 'Wordpress', 'PHP', 
+   'Symphony', 'Photoshop', 'Illustrator',
+   'InDesign', 'Adobe XD'
+];
+
+var tagCloud = TagCloud('.content', myTags,{
+
+ // radius in px
+ radius: 250,
+
+ // animation speed
+ // slow, normal, fast
+ maxSpeed: 'fast',
+ initSpeed: 'fast',
+
+ // 0 = top
+ // 90 = left
+ // 135 = right-bottom
+ direction: 135,
+ 
+ // interact with cursor move on mouse out
+ keep: true
+ 
+});
+
+//To change the color of text randomly
+var colors = ['#34A853', '#FBBC05', '#4285F4', '#7FBC00', 'FFBA01', '01A6F0'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+document.querySelector('.content').style.color = random_color;
