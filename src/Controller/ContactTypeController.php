c4a805d14e663ace09063b849c2fd4dbd9902b7e
Charles Letang <94009414+Yamazaki-Ryuji@users.noreply.github.com>
2022-06-17 20:22:40 +0200

    CSS

--- src/Controller/ContactTypeController.php
+++ src/Controller/ContactTypeController.php
@@ -17,8 +17,6 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\Type;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Hostname;
-use Symfony\Component\Validator\Constraints\Collection;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 use Symfony\Component\Validator\Exception\UnexpectedValueException;
 use Symfony\Component\Validator\Exception\InvalidArgumentException;
